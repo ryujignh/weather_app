565514935f46e31c1edf4e447b5ebb3ee7e149c9
ryujignh <ryujignh@gmail.com>
2022-02-10 21:57:20 -0800

    WIP: Adding CORS proxy

--- src/store/actions/locationActions.ts
+++ src/store/actions/locationActions.ts
@@ -3,18 +3,26 @@ import {RootState} from "../index";
 // import {GET_WEATHER, SET_ERROR, SET_LOADING, WeatherAction, WeatherData, WeatherError} from "../types";
 import {GET_LOCATIONS, SET_ERROR, SET_LOADING, LocationAction, LocationData, LocationError } from "../types";
 
+// const cors_proxy = require('cors-anywhere');
 export const getLocation = (city: string): ThunkAction<void, RootState, null, LocationAction> => {
   return async dispatch => {
     try {
+      // const host = process.env.HOST || '0.0.0.0';
+      // Listen on a specific port via the PORT environment variable
+      // const port = process.env.PORT || 8080;
+      // const cors_proxy = require('cors-anywhere');
+      // cors_proxy.createServer({
+      //   originWhitelist: [],
+      //   requireHeader: ['origin', 'x-requested-with'],
+      //   removeHeaders: ['cookie', 'cookie2']
+      // });
 
-      const res = await fetch(`https://www.metaweather.com/api/location/search/?query=${city}`);
+      const res = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${city}`);
 
       if (!res.ok) {
         const resData: LocationError = await res.json();
         throw new Error(resData.message);
       }
-
-      debugger
 
       const resData: LocationData = await res.json();
       dispatch({
